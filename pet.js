bab5a962c673bd7817e84238812484406abaeb8f
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-04-28 12:50:02 -0400

    Update pet.js

--- pet.js
+++ pet.js
@@ -1,1 +1,32 @@
+let hunger = 5;
+let energy = 5;
+let happiness = 5;
 
+function hunger() {
+  hunger -= 1;
+  console.log("You fed your pet! Hunger is now " + hunger);
+}
+
+if ( hunger <= 0) {
+  console.log("Your pet is full and doesn't want to eat more!");
+}
+
+function playPet() {
+  if (energy > 0) {
+    energy -= 1;
+    happiness += 1;
+    console.log("You played with your pet! Energy: " + energy + ", Happiness: " + happiness);
+  } else {
+    console.log("Your pet is too tired to play.");
+  }
+}
+
+function sleepPet() {
+  energy += 2;
+  console.log("Your pet took a nap! Energy is now " + energy);
+}
+
+setInterval(function() {
+  hunger += 1;
+  console.log("Your pet is getting hungrier! Hunger is now " + hunger);
+}, 3000);
